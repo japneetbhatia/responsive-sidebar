da1bf27c2c9282c6a9f983794b854e2634affbc8
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-10-25 00:39:23 +0530

    sidebar

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let btn = document.querySelector("#button")
+let btn = document.querySelector("#button");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".sbx-search-alt");
+
+btn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
+
+searchBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
